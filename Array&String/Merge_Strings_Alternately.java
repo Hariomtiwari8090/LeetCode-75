b4faace15b4886d018546f7b89f1f2de03d45d54
Hariomtiwari8090 <hariomtiwari.cse@gmail.com>
2025-09-28 02:17:14 +0530

    Update Merge_Strings_Alternately.java

--- Array&String/Merge_Strings_Alternately.java
+++ Array&String/Merge_Strings_Alternately.java
@@ -1,27 +1,20 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int a=word1.length();
-        int b=word2.length();
-        int i=Math.min(a,b);
-        String s="";
-        int j=0;
-        while(j!=i){
-            s+=word1.charAt(j);
-            s+=word2.charAt(j);
-            j++;
+        String ans="";
+        int i=0,j=0,k=0;
+        while(i<word1.length() && j<word2.length()){
+            if(k%2==0){
+                ans+=word1.charAt(i++);
+            }
+            else ans+=word2.charAt(j++);
+            k++;
         }
-        
-        if (a > b) {
-            while(j!=a){
-            s+=word1.charAt(j);
-            j++;
+        while(i<word1.length()){
+            ans+=word1.charAt(i++);
         }
-        } else if (b > a) {
-            while(j!=b){
-            s+=word2.charAt(j);
-            j++;
+        while(j<word2.length()){
+            ans+=word2.charAt(j++);
         }
-        }
-        return s;
+        return ans;
     }
 }
