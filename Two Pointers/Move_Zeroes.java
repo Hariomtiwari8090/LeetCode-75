fb82ddebe761a7e9daf983841285f141e3a529df
Hariomtiwari8090 <hariomtiwari.cse@gmail.com>
2025-10-03 21:40:53 +0530

    Create Move_Zeroes.java

---
+++ Two Pointers/Move_Zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int n=arr.length;
+        int i=0; 
+        for (int j=0;j<n;j++) {
+            if (arr[j]!=0) {
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+                i++;
+            }
+        }
+    }
+}
